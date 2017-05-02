46032063eade50bcb191841c57a074653cc7cbbb
liujie045 <liujie045.china@outlook.com>
2017-05-02 11:28:50 +0800

    Handy Commit

--- library/src/main/java/com/handy/selector/button/SmoothCompoundButton.java
+++ library/src/main/java/com/handy/selector/button/SmoothCompoundButton.java
@@ -16,39 +16,21 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Checkable;
 
-public abstract class SmoothCompoundButton extends Button implements Checkable {
+public abstract class SmoothCompoundButton extends android.support.v7.widget.AppCompatButton implements Checkable {
 
-	final String TAG = "SmoothCompoundButton";
 	private static final int ANIMATION_DURATION = 240;
-
-	private boolean mChecked;
-	private boolean mBroadcasting;
-	private OnCheckedChangeListener mOnCheckedChangeListener;
-	private OnCheckedChangeListener mOnCheckedChangeWidgetListener;
-
+	private static final int[] CHECKED_STATE_SET = {android.R.attr.state_checked};
+	/**
+	 * 用于动画，ANIMATION_STEP_PER_MS， 每一毫秒mCheckFraction需要改变的量
+	 **/
+	protected final float ANIMATION_STEP_PER_MS = 1f / ANIMATION_DURATION;
+	final String TAG = "SmoothCompoundButton";
 	/** ON状态的颜色，appcompat中是?attr/colorAccent **/
 	private final int DEFAULT_COLOR_ON = 0xff009688;
 	/** OFF状态的颜色，appcompat是colorControlNormal=?android:attr/textColorSecondary **/
 	private final int DEFAULT_COLOR_OFF = 0x8a000000;
-	private SmoothMarkDrawer mMarkDrawer;
-	private RectF mMarkBounds;
-	/** checked = false时mCheckFraction是0，true是1，动画过程由0到1之间 **/
-	private float mCheckFraction = 0f;
-	/** 用于动画，ANIMATION_STEP_PER_MS， 每一毫秒mCheckFraction需要改变的量 **/
-	protected final float ANIMATION_STEP_PER_MS = 1f / ANIMATION_DURATION;
-	/** 用于动画，上次draw的系统时间 */
-	private long mLastDrawTime = SystemClock.uptimeMillis();
-	// private final float mCheckFractionAnimationStep = 0.075f;// 0.075f;
-	/** 如果为true，则仅可以点击mark部分来toogle，点击文字部分无效 **/
-	private boolean mClickMarkOnly;
-	/** 是否反转SmoothMarkDrawer默认的mark位置**/
-	private boolean mReverseMarkPosition;  
-
-	private static final int[] CHECKED_STATE_SET = { android.R.attr.state_checked };
-	private boolean mIsAttachedToWindow = false;
 	// 这个ATTRS的项必须是从小到大排列的
 	private final int[] ATTRS = new int[] {
 			// android.R.attr.enabled, //16842766:0 //
@@ -60,11 +42,35 @@ public abstract class SmoothCompoundButton extends Button implements Checkable {
 			android.R.attr.checked,// 16843014
 			android.R.attr.adjustViewBounds,// 16843038 借来的attr，用于设置mClickMarkOnly
 			android.R.attr.tint, // 16843041 借来的attr，用于设置mark的颜色
-			android.R.attr.cropToPadding // 16843043 借来的attr，用于设置mark的左右位置是否反转 
+			android.R.attr.cropToPadding // 16843043 借来的attr，用于设置mark的左右位置是否反转
 
 	// android.R.attr.text// 16843087:6
 
 	};
+	private boolean mChecked;
+	private boolean mBroadcasting;
+	private OnCheckedChangeListener mOnCheckedChangeListener;
+	private OnCheckedChangeListener mOnCheckedChangeWidgetListener;
+	private SmoothMarkDrawer mMarkDrawer;
+	private RectF mMarkBounds;
+	// private final float mCheckFractionAnimationStep = 0.075f;// 0.075f;
+	/**
+	 * checked = false时mCheckFraction是0，true是1，动画过程由0到1之间
+	 **/
+	private float mCheckFraction = 0f;
+	/**
+	 * 用于动画，上次draw的系统时间
+	 */
+	private long mLastDrawTime = SystemClock.uptimeMillis();
+	/**
+	 * 如果为true，则仅可以点击mark部分来toogle，点击文字部分无效
+	 **/
+	private boolean mClickMarkOnly;
+	/**
+	 * 是否反转SmoothMarkDrawer默认的mark位置
+	 **/
+	private boolean mReverseMarkPosition;
+	private boolean mIsAttachedToWindow = false;
 
 	public SmoothCompoundButton(Context context) {
 		this(context, null);
@@ -78,6 +84,11 @@ public abstract class SmoothCompoundButton extends Button implements Checkable {
 	public SmoothCompoundButton(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		init(context, attrs);
+	}
+
+	// copy from support
+	public static boolean isLayoutRtl(View view) {
+		return ViewCompat.getLayoutDirection(view) == ViewCompat.LAYOUT_DIRECTION_RTL;
 	}
 
 	private void init(Context context, AttributeSet attrs) {
@@ -168,6 +179,14 @@ public abstract class SmoothCompoundButton extends Button implements Checkable {
 		return false;
 	}
 
+	void setCheckedStateByFraction() {
+		setChecked(mCheckFraction >= 0.5f);
+	}
+
+	float getFractionInternal() {
+		return mCheckFraction;
+	}
+
 	void setFractionInternal(float fraction) {
 		log("setFractionInternal->" + fraction);
 		if (fraction < 0) {
@@ -179,14 +198,6 @@ public abstract class SmoothCompoundButton extends Button implements Checkable {
 			mCheckFraction = fraction;
 			invalidate();
 		}
-	}
-
-	void setCheckedStateByFraction() {
-		setChecked(mCheckFraction >= 0.5f);
-	}
-
-	float getFractionInternal() {
-		return mCheckFraction;
 	}
 
 	@Override
@@ -403,6 +414,8 @@ public abstract class SmoothCompoundButton extends Button implements Checkable {
 		}
 	}
 
+	// Code for Checkable//
+
 	@Override
 	protected void onDetachedFromWindow() {
 		super.onDetachedFromWindow();
@@ -410,14 +423,6 @@ public abstract class SmoothCompoundButton extends Button implements Checkable {
 		if (mMarkDrawer != null) {
 			mMarkDrawer.onDetachedFromWindow(this);
 		}
-	}
-
-	// Code for Checkable//
-
-	@Override
-	public void setChecked(boolean checked) {
-		log("setChecked->" + checked);
-		setChecked(checked, true, true);
 	}
 
 	/**
@@ -466,6 +471,12 @@ public abstract class SmoothCompoundButton extends Button implements Checkable {
 	}
 
 	@Override
+	public void setChecked(boolean checked) {
+		log("setChecked->" + checked);
+		setChecked(checked, true, true);
+	}
+
+	@Override
 	public void toggle() {
 		setChecked(!mChecked);
 	}
@@ -491,17 +502,12 @@ public abstract class SmoothCompoundButton extends Button implements Checkable {
 		mOnCheckedChangeWidgetListener = listener;
 	}
 
-	public static interface OnCheckedChangeListener {
-		void onCheckedChanged(SmoothCompoundButton buttonView, boolean isChecked);
-	}
-
-	// copy from support
-	public static boolean isLayoutRtl(View view) {
-		return ViewCompat.getLayoutDirection(view) == ViewCompat.LAYOUT_DIRECTION_RTL;
-	}
-
 	private void log(String msg) {
 		Log.d(TAG, msg);
+	}
+
+	public interface OnCheckedChangeListener {
+		void onCheckedChanged(SmoothCompoundButton buttonView, boolean isChecked);
 	}
 
 }
