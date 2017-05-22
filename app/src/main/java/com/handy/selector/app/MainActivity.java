289c42d368f309ccf47b855e987c0c4d067b6450
liujie045 <liujie045.china@outlook.com>
2017-05-22 14:05:03 +0800

    Handy Commit

--- app/src/main/java/com/handy/selector/app/MainActivity.java
+++ app/src/main/java/com/handy/selector/app/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.handy.selector.app.button.SmoothButtonActivity;
 import com.handy.selector.app.dialog.DialogActivity;
 import com.handy.selector.app.materialdesign.demo.MaterialDesignActivity;
 import com.handy.selector.app.picker.PickerActivity;
@@ -17,13 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.Button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SmoothButtonActivity.class);
-                startActivity(intent);
-            }
-        });
         findViewById(R.id.Dialog).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
